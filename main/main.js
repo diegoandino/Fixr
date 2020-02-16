223ea8e54a9bd6fa1c1e64aab891e96daf62a5f8
Diego Andino <andino.diego98@gmail.com>
2020-02-16 14:27:08 -0700

    Added delete all function, updated fonts, added timestamp of posts, and fixed tags

--- main/main.js
+++ main/main.js
@@ -4,15 +4,19 @@ function saveBug(e) {
     var bugDesc = document.getElementById('bugDescInput').value;
     var bugPriority = document.getElementById('bugPriorityInput').value;
     var bugAssignedTo = document.getElementById('bugAssignedToInput').value;
+    var bugTag = document.getElementById('bugTagInput').value;
     var bugId = chance.guid();
     var bugStatus = 'Open';
+    var datePosted = getDate(new Date);
 
     var bug = {
         id: bugId,
         description: bugDesc,
         priority: bugPriority,
         assignedTo: bugAssignedTo,
-        status: bugStatus
+        status: bugStatus,
+        tag: bugTag,
+        date: datePosted 
     }
 
     //Check if Bug Description or Assigned Person tab is empty
@@ -50,13 +54,16 @@ function getBugs(canPost) {
         var priority = bugs[i].priority;
         var assignedTo = bugs[i].assignedTo;
         var status = bugs[i].status;
+        var tag = bugs[i].tag;
+        var date = bugs[i].date;
 
         bugList.innerHTML += '<div class="well">' + 
                               '<h6>Issue ID:' + id + '</h6>' +
-                              '<p><span class="label label-info">' + status + '</span></p>' +
+                              '<p><span class="label label-info">' + status + '</span> <span class="label label-info">' + tag + '</span></p>' +
                                '<h3>' + desc + '</h3>' +
-                               '<p><span class="glyphicon glyphicon-time"></span>' + priority + '</p>' +
-                               '<p><span class="glyphicon glyphicon-user"></span>' + assignedTo + '</p>' +
+                               '<p><span class="glyphicon glyphicon-time"></span>' + " Date posted: " + date + " " + '</p>' +
+                               '<p><span class="glyphicon glyphicon-exclamation-sign"></span>' + " Priority: " + priority + '</p>' +
+                               '<p><span class="glyphicon glyphicon-user"></span>' + " " + assignedTo + '</p>' +
                                '<a href="#" onClick="setStatusClosed(\''+ id + '\')" class="btn btn-warning">Close</a> ' +
                                '<button type="button" href="#" onClick="deleteBug(\''+ id + '\')" class="btn btn-danger">Delete</button>' +
                                '</div>';
@@ -64,6 +71,7 @@ function getBugs(canPost) {
 }
 
 
+// Close bug status
 function setStatusClosed(id) {
     var bugs = JSON.parse(localStorage.getItem('bugs'));
 
@@ -77,6 +85,7 @@ function setStatusClosed(id) {
 }
 
 
+// Delete individual bugs
 function deleteBug(id) {
     var bugs = JSON.parse(localStorage.getItem('bugs'));
 
@@ -88,3 +97,32 @@ function deleteBug(id) {
     localStorage.setItem('bugs', JSON.stringify(bugs));
     getBugs();
 }
+
+
+// Delete all bugs in list
+function deleteAllBugs(id) {
+    var bugs = JSON.parse(localStorage.getItem('bugs'));
+
+    //Get container for resetting padding (in the works. . .)
+    var container = document.getElementsByClassName("container").style;
+
+    //special case if id is undefined
+    if (bugs.id == null)
+        bugs.splice(0);
+
+    for (let i = 0; i < bugs.length; i++) {
+        bugs[i] = bugs.splice(0, bugs.length);
+    }
+
+    localStorage.setItem('bugs', JSON.stringify(bugs));
+    getBugs();
+    container.position = "fixed";
+}
+
+
+// Returns date for bug posts
+function getDate(date) {
+    var options = {  weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour: '2-digit', minute: '2-digit', second: '2-digit', hour12: true };
+
+    return new Date().toLocaleTimeString('en-us', options);
+  }
